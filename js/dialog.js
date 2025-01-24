b04954abbb75b89bcff8fa9cdfc1a31a2de8b473
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2025-01-24 19:47:19 +0800

    Remove dialog.html and associated assets; update story.txt with new narrative and format; refactor dialog.js for improved structure and functionality; modify dialog.css for layout adjustments.

--- js/dialog.js
+++ js/dialog.js
@@ -1,15 +1,23 @@
 // Constants
-const dialog = document.getElementById('dialog');
-const background = document.getElementById('bg');
-const startButton = document.getElementById('startbutton');
-var audios = {};
-var imgs = {};
+
 
 class DialogSystem {
     constructor() {
         this.lineNum = 0;
         this.isLocked = true;
         this.text = []; // 清空初始對話
+
+        this.audios = {};
+        this.imgs = {};
+
+        this.dialog = document.createElement("div");
+        document.getElementsByTagName("main")[0].appendChild(this.dialog);
+        this.dialog.id = "dialog";
+        this.dialog.appendChild(document.createElement("p"));
+
+        this.background = this.createElement('img', 'bg');
+        this.imgfile = this.createElement('span', 'imgfile');
+        this.audiofile = this.createElement('span', 'audiofile');
 
         // 讀取txt檔案
         fetch('story.txt')
@@ -22,12 +30,19 @@ class DialogSystem {
             })
             .catch(error => console.error('Error loading story:', error));
 
-        dialog.addEventListener('click', () => {
+        this.dialog.addEventListener('click', () => {
             if (!this.isLocked && this.lineNum < this.text.length) {
                 this.isLocked = true;
                 this.showWords(this.lineNum);
             }
         });
+    }
+
+    createElement(tag, id) {
+        const element = document.createElement(tag);
+        element.id = id;
+        this.dialog.appendChild(element);
+        return element; // Return the created element for later use
     }
 
     // Show words individually
@@ -53,7 +68,7 @@ class DialogSystem {
             }
             display += word;
             await new Promise(r => setTimeout(r, 10));
-            dialog.getElementsByTagName("p")[0].innerHTML = display;
+            this.dialog.getElementsByTagName("p")[0].innerHTML = display;
         }
         this.isLocked = false;
         this.lineNum += 1;
@@ -65,11 +80,11 @@ class DialogSystem {
         switch (params[0]) {
             case 'show':
                 // [show]
-                dialog.style.display = 'initial';
+                this.dialog.style.display = 'initial';
                 break;
             case 'hide':
                 // [hide]
-                dialog.style.display = 'none';
+                this.dialog.style.display = 'none';
                 break;
             case 'n':
                 // [newline]
@@ -77,19 +92,19 @@ class DialogSystem {
             case 'bg':
                 // background src object-fit
                 if (params[1] == 0) {
-                    background.style.visibility = 'hidden';
+                    this.background.style.visibility = 'hidden';
                 } else {
-                    background.src = 'background/' + params[1] + '.jpg';
-                    background.style.visibility = 'visible';
-                    background.style.objectFit = params[2];
+                    this.background.src = 'resources/' + params[1];
+                    this.background.style.visibility = 'visible';
+                    this.background.style.objectFit = params[2];
                 }
                 break;
             case 'img':
                 // [img name src x y z width height show]
-                let img = imgs[params[1]] ? imgs[params[1]] : document.createElement('img');
+                let img = this.imgs[params[1]] ? this.imgs[params[1]] : document.createElement('img');
                 // 創建圖片元素
-                document.getElementById("imgfile").appendChild(img);
-                img.src = "image/" + params[2] + ".jpg";
+                this.imgfile.appendChild(img);
+                img.src = "resources/" + params[2];
                 // Set image styles (1920x1080 grid)
                 Object.assign(img.style, {
                     left: parseFloat(params[3]) / 1920 * 100 + '%',
@@ -98,9 +113,9 @@ class DialogSystem {
                     height: parseFloat(params[7]) / 1080 * 100 + '%'
                 });
                 img.style.zIndex = parseFloat(params[5]);
-                imgs[params[1]] = img; // 將圖片元素和參數存入字典
+                this.imgs[params[1]] = img; // 將圖片元素和參數存入字典
                 if (params[8] == '0') {
-                    imgs[params[1]].style.display = "none";
+                    this.imgs[params[1]].style.display = "none";
                 } else {
                     img.style.display = "initial" // 開始顯示圖片
                 }
@@ -109,34 +124,34 @@ class DialogSystem {
                 // [audio name src play time(s) fade(ms)]
                 if (params[3] == 'play') {
                     let audio = document.createElement('audio'); // 創建音頻元素
-                    document.getElementById("audiofile").appendChild(audio);
-                    audio.src = "background/" + params[2] + ".mp3";
+                    this.audiofile.appendChild(audio);
+                    audio.src = "resources/" + params[2];
                     audio.play(); // 開始播放音頻
-                    audios[params[1]] = audio; // 將音頻元素和參數存入字典
+                    this.audios[params[1]] = audio; // 將音頻元素和參數存入字典
                     if (params[4] != 0) {
-                        setInterval(() => { audios[params[1]].pause(); }, parseInt(params[4]) * 1000)
+                        setInterval(() => { this.audios[params[1]].pause(); }, parseInt(params[4]) * 1000)
                     }
                     if (params[5] != 0) {
                         let volumeIncrement = parseInt(params[5])/100;
-                        audios[params[1]].volume = 0;
+                        this.audios[params[1]].volume = 0;
                         for (let i = 0; i < 100; i++) {
                             setTimeout(() => {
-                                audios[params[1]].volume += 0.01;
+                                this.audios[params[1]].volume += 0.01;
                             }, i*volumeIncrement);
                         }
                     }
                 } else {
                     if (params[5] != 0) {
                         let volumeIncrement = parseInt(params[5])/100;
-                        audios[params[1]].volume = 1;
+                        this.audios[params[1]].volume = 1;
                         for (let i = 0; i < 100; i++) {
                             setTimeout(() => {
-                                audios[params[1]].volume -= 0.01;
+                                this.audios[params[1]].volume -= 0.01;
                             }, i*volumeIncrement);
                         }
-                        setInterval(()=>{audios[params[2]].pause();},parseInt(params[5]))
+                        setInterval(()=>{this.audios[params[2]].pause();},parseInt(params[5]))
                     }else{
-                        audios[params[2]].pause(); 
+                        this.audios[params[2]].pause(); 
                     }
 
                 }
@@ -147,17 +162,4 @@ class DialogSystem {
         }
         return "";
     }
-}
-
-// Event listeners
-window.onload = () => {
-    dialog.style.display = 'none';
-    background.src = "background/resize.png";
-}
-startButton.onclick = () => {
-    dialog.style.display = 'initial';
-    background.style.visibility = "hidden";
-    background.src = "";
-    startButton.style.display = 'none';
-    const dialogSystem = new DialogSystem();
 }
