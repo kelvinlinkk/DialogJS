974e735ba2f0c0dfec0a78c0a2de58accbf423bd
Kun-Yi Lin <108348635+kelvinlinkk@users.noreply.github.com>
2025-01-25 09:33:41 +0800

    fix gitignore

--- js/flow.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    const startButton = document.getElementById('startbutton');
-    startButton.onclick = () => {
-        const dialogSystem = new DialogSystem();
-        startButton.style.display = 'none';
-    },{once:true};
-}
